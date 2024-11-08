fd267d23c97ed1899a93096792c3c622d5441eef
N1CKS-35 <00001118082217sp@al.educacao.sp.gov.br>
2024-11-08 10:39:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'é uma linguagem de programação '
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capital dos Estados Unidos da América?',
+    'Washington D.C'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz outono em Inglês?',
+    'Outono em ingles é autun ou fall (autun/faul)'
+)
